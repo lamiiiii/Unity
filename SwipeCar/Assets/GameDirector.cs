79f36b46a7557440764f3bd36cf710173a4cd6b8
lamiiiii <115445005+lamiiiii@users.noreply.github.com>
2024-01-16 23:03:08 +0900

    Commit

---
+++ SwipeCar/Assets/GameDirector.cs
@@ -0,0 +1,50 @@
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.UI; // UI를 사용하는 데 필요하다.
+
+//public class GameDirector : MonoBehaviour
+//{
+//    GameObject car;
+//    GameObject flag;
+//    GameObject distance;
+
+//    void Start()
+//    {
+//        this.car = GameObject.Find("car");
+//        this.flag = GameObject.Find("flag");
+//        this.distance = GameObject.Find("Distance");
+//    }
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        float length = this.flag.transform.position.x - this.car.transform.position.x;
+//        this.distance.GetComponent<Text>().text = "목표 지점까지 " + length.ToString("F2") + "m";
+//    }
+//}
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameDirector: MonoBehaviour
+{
+    GameObject car;
+    GameObject flag;
+    GameObject distance;
+
+    void Start()
+    {
+        this.car = GameObject.Find("car");
+        this.flag = GameObject.Find("flag");
+        this.distance = GameObject.Find("Distance");
+    }
+
+    void Update()
+    {
+        float length = this.flag.transform.position.x - this.car.transform.position.x;
+        this.distance.GetComponent<Text>().text = "목표 지점까지 " + length.ToString("F2") + "m";
+    }
+}
